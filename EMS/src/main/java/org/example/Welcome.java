9c5e2a021645d2db415cd5d5ac67cf61b82a975b
Muturi-002 <martinmnjoroge03@gmail.com>
2025-04-01 18:42:22 +0300

    Added HR directory with file in HR branch; added test file Welcome.java

---
+++ EMS/src/main/java/org/example/Welcome.java
@@ -0,0 +1,13 @@
+package org.example;
+import java.awt.*;
+import javax.swing.*;
+import java.awt.event.*;
+
+public class Welcome extends JFrame{
+    public Welcome(){
+
+    }
+    public static void main (String [] args){
+
+    }
+}
